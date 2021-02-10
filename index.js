302d4289baf87ec27ddc7eac78dc34f9b1c16ec9
Patrick Cartlidge <patrickcartlidge@gmail.com>
2021-02-10 21:17:20 +0000

    adding history

--- index.js
+++ index.js
@@ -16,22 +16,30 @@ const io = socketIO(server);
 io.on('connection', (socket) => {
   console.log('connected');
   socket.on('disconnect', () => console.log('goodbye'));
+
+  socket.on('message_history_request', (last_msg_id) => {
+	const channel = client.guilds.cache.get('514879056433381409').channels.cache.get('514879057868095489');
+	channel.messages.fetch({ limit: 5, ...(last_msg_id ? { before: last_msg_id } : {})}, false, false)
+	.then(messages => { io.emit('message_history_request_response', { messages: messages.map(msg => messageToMsg(msg)) }) })
+	.catch(console.error)
+	console.log(socket);
+	});
 });
 
 setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
 
 const client = new Discord.Client();
 
-client.on('message', msg => {
-	if (msg.content === 'ping') {
-		msg.reply('Pong!');
-	}
+const messageToMsg = msg => ({
+	content: msg.content, user: msg.author.username, datestamp: msg.createdTimestamp, id: msg.id
+});
 
-	io.emit('message', { content: msg.content, user: msg.author.username });
+client.on('message', msg => {
+	io.emit('message', messageToMsg(msg));
 });
 
 client.on('ready', () => {
-	console.log(`Logged in as ${client.user.tag}`)
+	console.log('Bot is connected...')
 });
 
 client.login(process.env.DISCORD_KEY);

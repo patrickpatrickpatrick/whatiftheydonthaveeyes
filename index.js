4178117bc2b3c0043569118b305e290f1b8daaaf
Patrick Cartlidge <patrickcartlidge@gmail.com>
2021-02-10 10:44:58 +0000

    initial commit, prepping for heroku test deploy

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const socketIO = require('socket.io')
+const Discord = require('discord.js');
+
+const PORT = process.env.PORT || 3000;
+const INDEX = '/index.html';
+
+const server = express()
+  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+const io = socketIO(server);
+
+io.on('connection', (socket) => {
+  console.log('connected');
+  socket.on('disconnect', () => console.log('goodbye'));
+});
+
+setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+
+const client = new Discord.Client();
+
+client.on('message', msg => {
+	if (msg.content === 'ping') {
+		msg.reply('Pong!');
+	}
+	io.emit('time', msg.content);
+});
+
+client.on('ready', () => {
+	console.log(`Logged in as ${client.user.tag}`)
+});
+
+client.login('ODA4NDI0Mjc0MDQ5NDk5MTU2.YCGVxg.cET8NOZzRpnWFyVOGghK9GEw9Jo');
